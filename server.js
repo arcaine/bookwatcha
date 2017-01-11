16b51b4197301be2a2753d3d5e8f07c0a58f4c2e
arcaine <arcaine34@gmail.com>
2017-01-11 14:58:04 +0900

    기본 모듈 설치 & 파일 구조 작업

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var session = require('express-session');
+var MySQLStore =require('express-mysql-session')(session);
+var bodyParser = require('body-parser');
+
+var app =express();
+//jade view 파일 이쁘게
+app.locals.pretty = true;
+
+//jade 설정
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: '1234DSFs@adf1234!@#$asd',
+  resave: false,
+  saveUninitialized: true,
+  store:new MySQLStore({
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password:'111111',
+    database:'o2'
+  })
+}));
+
+
+app.get('/', function(req, res){
+  res.render("view")
+});
+
+
+app.listen(3000, function(){
+  console.log('Connected, 3000 Port!')
+});
